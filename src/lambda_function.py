4e14190c844fb7af41159aaf2dee2a0284909974
heerman <lwheerman@gmail.com>
2020-08-11 16:24:09 -0400

    file rename

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,6 +1,6 @@
 import json
 import request_utils
-import endpoints_scripts
+import endpoints_users
 
 def lambda_handler(event, context):
     """Main entry point for the web service"""
@@ -10,13 +10,13 @@ def lambda_handler(event, context):
     resp = {'statusCode': 500}  # default response, to be overwritten
 
     # Each endpoint calls a function
-    if mthd == 'GET' and path == '/services/scripts/getlicenses':
-        resp = endpoints_scripts.get_licenses()
+    if mthd == 'GET' and path == '/services/users/getlicenses':
+        resp = endpoints_users.get_licenses()
 
-    elif mthd == 'POST' and path == '/services/scripts/getuserinfo':
+    elif mthd == 'POST' and path == '/services/users/getuserinfo':
         username = request_utils.post_mfd_parameter(event, 'username')
         password = request_utils.post_mfd_parameter(event, 'password')
-        resp = endpoints_scripts.get_user_info(username, password)
+        resp = endpoints_users.get_user_info(username, password)
 
     # Add headers to http response
     if 'headers' not in resp:
